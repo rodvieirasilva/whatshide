4bf52157026609e08b1187dd360e222f0884d745
rodvieirasilva <rodvieirasilva@gmail.com>
2016-12-15 10:53:04 -0200

    [UPDATE] Atualizado forma de tradução e adicionado link no logo

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,5 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
+// Copyright (c) 2016 Lelis Alves, Rodrigo Vieira. All rights reserved.
+// Use of this source code is governed by a Apache license that can be
 // found in the LICENSE file.
 
 /**
@@ -11,43 +11,25 @@
 
 (function(){
 
-  angular.module('blockWhatsApp', [])
-  .controller('PopupController',  ['$scope',function($scope) {
-    var popupController = this;
+  var app = angular.module('blockWhatsApp', []);
+  
+  app.controller('PopupController',  ['$scope', function($scope) {
+	var popupController = this;
+	
+	$scope.desc 			= chrome.i18n.getMessage("scopeDesc");
+	$scope.btn_clear 		= chrome.i18n.getMessage("scopeBtnClear");
+	$scope.btn_clearall 	= chrome.i18n.getMessage("scopeBtnClearAll");
+	$scope.btn_changepw 	= chrome.i18n.getMessage("scopeBtnChangepw");
+	$scope.place_newword	= chrome.i18n.getMessage("scopePlaceNewWord");
+	$scope.place_newpw		= chrome.i18n.getMessage("scopePlaceNewpw");
+	$scope.err_wrong		= chrome.i18n.getMessage("scopeErrWrong");
 	  
-	  if(navigator.browserLanguage) {  
-		  var lang = navigator.browserLanguage;    
-	  }
-	  else if(navigator.language) {
-		  var lang = navigator.language;
-	  }
- 
-      if(lang == 'pt-BR'){
-		  $scope.desc = 'Use os controles para esconder as conversas';
-		  $scope.btn_clear = 'Limpar';
-		  $scope.btn_clearall = 'Limpar Todos';
-		  $scope.btn_changepw = 'Adicionar/Mudar Senha';
-		  $scope.place_newword= 'Digite uma nova palavra aqui';
-		  $scope.place_newpw= 'Senha aqui';
-		  $scope.err_wrong= 'Senha inválida!';	  		  
-	  }
-	  else{
-	  	  $scope.desc = 'Use the toggle button to hide/show chats';
-		  $scope.btn_clear = 'Clear';
-		  $scope.btn_clearall = 'Clear All';
-		  $scope.btn_changepw = 'Add/Change Password';
-		  $scope.place_newword= 'Input new word here';
-		  $scope.place_newpw= 'Password here';
-		  $scope.err_wrong= 'Password inavelid!';
-	  }
-	
-	
-	popupController.btnPassawordText = "Save";
+	popupController.stateBtnPassaword = 1;//"Save";
 	popupController.showError = false;  
-	
-    popupController.words = [{value: 'familia', active: true}];
 
-    chrome.storage.sync.get('words',
+	popupController.words = [{value: 'familia', active: true}];
+
+	chrome.storage.sync.get('words',
 		function (item){
 
 		  if( Object.prototype.toString.call( item.words ) === '[object Array]' ) {
@@ -69,67 +51,69 @@
 				popupController.passwordOriginalValue = password;
 				popupController.showPassword = true;
 				popupController.showContent = false;
-				popupController.btnPassawordText = "Login";
+				popupController.stateBtnPassaword = 2;//"Login";
+				
+				//$scope.$emit('ui:focus', '#password');
+			}
+			else
+			{
+				//$scope.$emit('ui:focus', '#word');
 			}
 			$scope.$apply();
 		});
 
-    popupController.addWord = function() {
+	popupController.addWord = function() {
 		popupController.wordValue = popupController.wordValue.trim();
 		if(popupController.wordValue.length > 1)
-		{
-			
+		{			
 			popupController.words.push({value:popupController.wordValue, active:true});
 			popupController.wordValue = '';
 			popupController.saveChanges();
 		}
 
-    };
+	};
 
-    popupController.saveChanges = function()
-    {
-      chrome.storage.sync.set({'words': popupController.words }, function() {
+	popupController.saveChanges = function()
+	{
+	  chrome.storage.sync.set({'words': popupController.words }, function() {
 
-        $scope.$apply();
-
-      });
-    }
+	  });
+	}
 	popupController.addPassword = function()
-    {
+	{
 	 
 	  popupController.showContent = false;
 	  popupController.showPassword = true;
 	  popupController.showError = false; 	 
-	  popupController.btnPassawordText = "Save";
-	  $scope.$apply();
-    }
-	
+	  popupController.stateBtnPassaword = 1;//"Save";
+	}
+
 	popupController.addPasswordValue = function()
-    {
-		if(popupController.btnPassawordText == "Login")
+	{
+		if(popupController.stateBtnPassaword == 2)//"Login"
 		{
 			if(popupController.passwordOriginalValue == popupController.passwordValue)
 			{
 				popupController.showContent = true;
 				popupController.showPassword = false;
 				popupController.showError = false;  
-				popupController.passwordValue = '';
+				popupController.passwordValue = '';				
+				//$scope.$emit('ui:focus', '#word');
 			}
 			else
 			{
 				popupController.showError = true;  
+				//$scope.$emit('ui:focus', '#password');
 			}
-			//$scope.$apply();
 		}
 		else{
 			chrome.storage.sync.set({'password': popupController.passwordValue }, function() {
-				$scope.$apply();
-				
 				popupController.showContent = true;
 				popupController.showPassword = false;
+				//$scope.$emit('ui:focus', '#word');
 				$scope.$apply();				
 			});
-	  }
+		}
 	  
 	  
     }
@@ -140,24 +124,29 @@
 
       popupController.saveChanges();
     };
-	
-    popupController.clearUncheckeds = function() {
-      var olds = popupController.words;
-      popupController.words = [];
-      angular.forEach(olds, function(old) {
-        if (old.active)
-        {
-          popupController.words.push(old);
-        }
-      });
-      popupController.saveChanges();
-    };
 
-    popupController.clearAll = function() {
-      popupController.words = [];
-      popupController.saveChanges();
-    };
+	popupController.clearUncheckeds = function() {
+	  var olds = popupController.words;
+	  popupController.words = [];
+	  angular.forEach(olds, function(old) {
+		if (old.active)
+		{
+		  popupController.words.push(old);
+		}
+	  });
+	  popupController.saveChanges();
+	};
+
+	popupController.clearAll = function() {
+	  popupController.words = [];
+	  popupController.saveChanges();
+	};
 
   }]);
 
 })();
+
+
+/*
+
+*/
